dd0d89b0f0f2170ece4a53f68ee00f93769e4672
Jack Mallers <jimmymowschess@gmail.com>
2013-12-12 14:20:49 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TFCV3Server::Application.config.secret_key_base = 'a1d9e6e989551d490c093071bc50ffb9a57f2c0ebfdb46439cc10e877f2bf6ae54493837b1600505d374a8281f824a6cdeac13dd5c6d255ac5dc7bc6b5aa3431'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TFCV3Server::Application.config.session_store :cookie_store, key: '_TFC_V3_server_session'
